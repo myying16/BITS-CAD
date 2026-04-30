301bbf27a14a81b89ad03338d384544ecbae0bc2
myying16 <169115968+myying16@users.noreply.github.com>
2026-04-30 15:06:42 +0800

    Update positional_encoding.py

--- model/layers/positional_encoding.py
+++ model/layers/positional_encoding.py
@@ -2,9 +2,7 @@ import math
 import torch
 import torch.nn as nn
 
-#基于正弦和余弦函数的固定编码
-# PE(pos, 2i) = sin(pos / 10000^(2i/d_model))
-# PE(pos, 2i+1) = cos(pos / 10000^(2i/d_model))
+
 class PositionalEncodingSinCos(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=250):
         super(PositionalEncodingSinCos, self).__init__()
@@ -14,14 +12,13 @@ class PositionalEncodingSinCos(nn.Module):
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        #调整维度：(max_len, d_model) → (max_len, 1, d_model) → (1, max_len, d_model)
+        
         pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe) #注册为缓冲区（不参与训练）
+        self.register_buffer('pe', pe)
     def forward(self, x):
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
-#基于查找表的可学习编码
 class PositionalEncodingLUT(nn.Module):
 
     def __init__(self, d_model, dropout=0.1, max_len=250):
@@ -35,9 +32,7 @@ class PositionalEncodingLUT(nn.Module):
     def _init_embeddings(self):
         nn.init.kaiming_normal_(self.pos_embed.weight, mode="fan_in")
 
-    def forward(self, x):  #[256, 100, 256]
+    def forward(self, x):  
         pos = self.position[:x.size(0)]
-        #print("x",x.shape)
-        #print("pos",pos.shape)
         x = x + self.pos_embed(pos)
         return self.dropout(x)
